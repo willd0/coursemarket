184e12783301279cbd05470de6e9513e0d327303
Will <willd2122@gmail.com>
2021-09-28 17:57:50 -0500

    updated the courses page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :sections
   resources :courses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  #root 'home#index'
+  root 'courses#index'
 end
