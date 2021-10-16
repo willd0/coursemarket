3a4f02ca42e5bc44f58b882327747d757a78cdeb
Will <willd2122@gmail.com>
2021-10-15 19:49:29 -0500

    added enrollment checking

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :courses, :dependent => :destroy
   has_many :enrollments
+  has_many :enrolled_courses, through: :enrollments, source: :course
+
+  def enrolled_in?(course)
+    return enrolled_courses.include?(course)
+  end
 end
