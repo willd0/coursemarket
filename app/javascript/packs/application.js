a31ea24b0d9b108c96a77e0722c8537eeec12b97
Will <willd2122@gmail.com>
2021-10-22 21:11:25 -0500

    fixed issue with turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 //= require jquery3
@@ -12,5 +12,5 @@ import "channels"
 //= require bootstrap-sprockets
 
 Rails.start()
-Turbolinks.start()
+
 ActiveStorage.start()
